e13bd772280c4084a44392957386bef8aa0fb0b2
AhmadAboElzahab-MBLM <aelzahab@mblm.com>
2025-08-19 10:10:29 +0400

    add gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
